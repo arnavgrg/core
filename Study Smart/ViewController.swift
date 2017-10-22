a54103a29b7267d4b1b88eab88ec7a3aa568bb93
parp1 <parth.pendurkar@gmail.com>
2017-10-21 22:08:17 -0700

    Initial Files

---
+++ Study Smart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study Smart
+//
+//  Created by Parth Pendurkar on 10/21/17.
+//  Copyright © 2017 UCLA DevX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
