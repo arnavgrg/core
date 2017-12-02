1c3f870c83bd3c9ad61d57de6e59c49527c97bd5
akumar010 <akumar2019@ucla.edu>
2017-12-02 14:30:09 -0800

    Created function to translate from busyness score to UIColor for the map pins

--- Study Smart/MapViewController.swift
+++ Study Smart/MapViewController.swift
@@ -155,6 +155,34 @@ extension MapViewController
         detailView.label.text = "TEST"
         detailView.isHidden = true
     }
+    
+    func getPinColor(busyness: Int) -> UIColor
+    {
+        let empty = 0x64B0CC
+        let not_busy = 0x5DB58F
+        let busy = 0xF7D64A
+        let very_busy = 0xF49B78
+        let packed = 0xF1562D
+        
+        var libColor: Int
+        switch busyness {
+        case 0..<25:
+            libColor = empty
+        case 25..<50:
+            libColor = not_busy
+        case 50..<70:
+            libColor = busy
+        case 70..<90:
+            libColor = very_busy
+        case 90..<101:
+            libColor = packed
+        default:
+            libColor = 0x000000
+        }
+        
+        return UIColor(rgb: libColor)
+        
+    }
 }
 
 @objc
@@ -227,5 +255,23 @@ extension MapViewController: CLLocationManagerDelegate
             // implement additional logic if needed...
         }
         // implement logic for other status values if needed...
+    }
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
     }
 }
